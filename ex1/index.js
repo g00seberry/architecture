d2a4ea4c2ca555539578b6c5075de4e3e3020300
gooseberry <gooseberry.plusplus@gmail.com>
2024-10-15 11:52:35 +0300

    complete

--- ex1/index.js
+++ ex1/index.js
@@ -1,3 +1,24 @@
-const testFn = () => 1;
+const EPS = 10e-7;
+function solve(a, b, c, e = EPS) {
+  [a, b, c, e].forEach((x) => {
+    if (typeof x !== "number" || Number.isNaN(x) || !Number.isFinite(x))
+      throw new Error("Error: wrong args.");
+  });
+  const isZero = (d) => d < Math.abs(e) && d > -Math.abs(e);
+  const correct = (x) => (isZero(x) ? 0 : x);
+  const [cA, cB, cC] = [correct(a), correct(b), correct(c)];
+  if (cA === 0) throw Error("Error: a === 0.");
+  const cD = correct(cB * cB - 4 * cA * cC);
+  if (cD < 0) return [];
+  const sqrtD = Math.sqrt(cD);
+  const a2 = a * 2;
+  if (cD === 0) return [-b / a2];
+  if (cD > 0) return [(-b + sqrtD) / a2, (-b - sqrtD) / a2];
+}
 
-module.exports = testFn;
+console.log(solve(1, 0, EPS / 10));
+
+module.exports = { solve, EPS };
+
+/**
+ */
--- ex1/tests/index.test.js
+++ ex1/tests/index.test.js
@@ -1,5 +1,34 @@
-const testFn = require("../index");
+const { EPS, solve } = require("../index");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(testFn()).toBe(1);
+test("x^2+1 = 0 корней нет", () => {
+  expect(solve(1, 0, 1)).toEqual([]);
+});
+test("x^2-1 = 0 есть два корня кратности 1", () => {
+  expect(solve(1, 0, -1)).toEqual([1, -1]);
+});
+test("x^2+2x+1 = 0 есть один корень кратности 2", () => {
+  expect(solve(1, 2, 1)).toEqual([-1]);
+});
+test("коэффициент a не может быть равен 0", () => {
+  expect(() => solve(0, 2, 1)).toThrow();
+});
+test("D находится в пределах  условного 0 (-e;+e)", () => {
+  expect(solve(1, 0, EPS / 10)[0] === -0).toEqual(true);
+});
+test("D находится за пределами условного 0 (-e;+e)", () => {
+  // < 0
+  expect(solve(1, 0, EPS)).toEqual([]);
+  // > 0
+  expect(solve(1, 0, -EPS)).toEqual([0.001, -0.001]);
+});
+test("аргументы конечные, типа number, != NAN", () => {
+  expect(() => solve(Infinity, 1, 1)).toThrow();
+  expect(() => solve(1, Infinity, 1)).toThrow();
+  expect(() => solve(1, 1, Infinity)).toThrow();
+  expect(() => solve("1", 1, 1)).toThrow();
+  expect(() => solve(1, "1", 1)).toThrow();
+  expect(() => solve(1, 1, "1")).toThrow();
+  expect(() => solve("num", 1, 1)).toThrow();
+  expect(() => solve(1, "num", 1)).toThrow();
+  expect(() => solve(1, 1, "num")).toThrow();
 });
